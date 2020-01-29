bf329ca70c521b1c1cc133c69132d9916eeb4603
Evan Klitzke <evan@eklitzke.org>
2020-01-29 12:59:26 -0800

    update layout

--- keyboards/ergodox_ez/keymaps/eklitzke/keymap.c
+++ keyboards/ergodox_ez/keymaps/eklitzke/keymap.c
@@ -36,10 +36,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LEFT,                                        KC_RIGHT,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
-    KC_LALT,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
+    KC_LALT,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           LT(1,KC_NO),                                    KC_TRANSPARENT, KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
     KC_LSHIFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           LT(1,KC_SCOLON),KC_QUOTE,
     KC_LCTRL,       LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,           KC_ESCAPE,                                      KC_PSCREEN,     KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,
-    LT(1,KC_GRAVE), TG(1),          LALT(KC_LSHIFT),KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    MO(1),
+    KC_GRAVE,       TG(1),          LALT(KC_LSHIFT),KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    MO(1),
                                                                                                     KC_LALT,        KC_LALT,        KC_RALT,        KC_RCTRL,
                                                                                                                     KC_HOME,        KC_PGUP,
                                                                                     KC_SPACE,       KC_BSPACE,      KC_END,         KC_PGDOWN,      KC_TAB,         KC_ENTER
