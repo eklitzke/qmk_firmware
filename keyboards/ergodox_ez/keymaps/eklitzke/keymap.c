0a4f5f6b8f7b4cf12b1bd50a3ef89c7fb39157ba
Evan Klitzke <evan@eklitzke.org>
2020-04-25 14:06:34 -0700

    disable that annoying key

--- keyboards/ergodox_ez/keymaps/eklitzke/keymap.c
+++ keyboards/ergodox_ez/keymaps/eklitzke/keymap.c
@@ -10,7 +10,6 @@
 #include "keymap_canadian_multilingual.h"
 #include "keymap_german_ch.h"
 #include "keymap_jp.h"
-#include "keymap_bepo.h"
 
 #define KC_MAC_UNDO LGUI(KC_Z)
 #define KC_MAC_CUT LGUI(KC_X)
@@ -39,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LALT,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 LT(1,KC_NO),    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
     KC_LSHIFT,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           LT(1,KC_SCOLON),KC_QUOTE,
     KC_LCTRL,       LCTL_T(KC_Z),   KC_X,           KC_C,           KC_V,           KC_B,           KC_ESCAPE,                                      KC_PSCREEN,     KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,
-    KC_GRAVE,       TG(1),          LALT(KC_LSHIFT),KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    MO(1),
+    KC_GRAVE,       KC_TRANSPARENT, LALT(KC_LSHIFT),KC_LEFT,        KC_RIGHT,                                                                                                       KC_UP,          KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    MO(1),
                                                                                                     KC_LALT,        KC_LALT,        KC_RALT,        KC_RCTRL,
                                                                                                                     KC_HOME,        KC_PGUP,
                                                                                     KC_SPACE,       KC_BSPACE,      KC_END,         KC_PGDOWN,      KC_TAB,         KC_ENTER
